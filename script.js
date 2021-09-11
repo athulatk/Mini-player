d5b2037db2ead4014d42005a61c2a068f795ce38
N Athul Kumar <athulnvinod@gmail.com>
2021-09-11 14:14:20 +0530

    first

---
+++ script.js
@@ -0,0 +1,71 @@
+const musicContainer=document.querySelector('.container')
+const playBtn=document.querySelector('#play')
+const prevBtn=document.querySelector('#prev')
+const nextBtn=document.querySelector('#next')
+const audio=document.querySelector('#audio')
+const progressContainer=document.querySelector('.progress-container')
+const progress=document.querySelector('.progress')
+const title=document.querySelector('#title')
+const cover=document.querySelector('#cover')
+
+const songs=['kgf','Scam1992','SpiderMan3']
+
+let songIndex=0;
+
+loadsong(songs[songIndex])
+
+function loadsong(song){
+    title.innerText=song
+    audio.src=`music/${song}.mp3`
+    cover.src=`imgs/${song}.jpg`
+}
+
+function playSong(){
+    musicContainer.classList.add('music-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
+    audio.play()
+}
+
+function pauseSong(){
+    musicContainer.classList.remove('music-play')
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+    audio.pause()
+}
+
+
+function updateProgress(e){
+    const {duration,currentTime} = e.srcElement
+    const progressPercent = (currentTime/duration)*100
+    progress.style.width=`${progressPercent}%`
+}
+
+function nextSong(){
+    songIndex++;
+    loadsong(songs[songIndex%songs.length]);
+    playSong();
+}
+
+function prevSong(){
+    songIndex--;
+    loadsong(songs[songIndex%songs.length]);
+    playSong();
+}
+
+//Event listeners
+playBtn.addEventListener('click',()=>{
+    const isPlaying=musicContainer.classList.contains('music-play')
+    if(isPlaying){
+        pauseSong()
+    }
+    else{
+        playSong()
+    }
+})
+
+nextBtn.addEventListener('click',nextSong);
+prevBtn.addEventListener('click',prevSong);
+
+audio.addEventListener('timeupdate',updateProgress)
+audio.addEventListener('ended',nextSong)
